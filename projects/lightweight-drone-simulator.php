64ae5757a5b0fc00d9df736ee4c25fde3465bd2a
Evan Putnam <quorumcode@gmail.com>
2024-03-28 13:44:49 -0700

    Changed paper link to IEEE page for paper

--- projects/lightweight-drone-simulator.php
+++ projects/lightweight-drone-simulator.php
@@ -24,12 +24,6 @@
             </p>
 
             <p class="individual-project-text">
-                &nbsp;The paper is in the process of being submitted to a conference and is
-                therefore still being changed to meet the conference requests. The downloadable
-                PDF is the latest version as of 10/08/2023.
-            </p>
-
-            <p class="individual-project-text">
                 &nbsp;As of 10/03/2023 the paper has been finalized and is in the process of being
                 published at the Software Designed Systems conference 2023. Information on the 
                 conference can be found <a href="https://emergingtechnet.org/SDS2023/" 
@@ -42,7 +36,7 @@
             </p>
             
             <div class="individual-project-links">
-                <a href="../downloads/REU__Lightweight_Drone_Simulator.pdf" class="btn" download=""> <h3> Download the paper </h3> </a>
+                <a href="https://ieeexplore.ieee.org/document/10329146" class="btn" target="_blank"> <h3> IEEE Page </h3> </a>
                 <a href="https://gitlab.com/reu-tamale/drone_simulator/dsim_2023" class="btn" target="_blank"> <h3> GitLab </h3> </a>
             </div>
         </section>
